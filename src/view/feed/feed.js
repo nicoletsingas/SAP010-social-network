94fd0b8900044274cc39803ab3987a8cb3ec0634
Nicole Tsingas <nicolecass27@gmail.com>
2023-06-15 17:20:53 -0300

    criado uma pasta para o header e reutilizado o elemento na página feed, ajustado rolagem do menu

--- src/view/feed/feed.js
+++ src/view/feed/feed.js
@@ -1,74 +1,20 @@
 import {
-  logOut,
   createPost,
   listAllPosts,
   editPost,
   deletePost,
 } from '../../firebase/firebase';
 import './feed.css';
-import profileIcon from '../../images/profile-icon.svg';
-import signoutIcon from '../../images/signout-icon.svg';
-import feedIcon from '../../images/feed-icon.svg';
+import header from '../header/header.js';
 
 export default (user) => {
+  const container = document.createElement('div');
+  container.appendChild(header());
+
   const containerFeed = document.createElement('section');
   containerFeed.classList.add('container-feed');
   const templateFeed = `
-    <header>
-      <div class="header">
-        <picture class="hamburger-menu">
-          <img class="icon-menu" src="images/menu-hamburger.svg" alt="menu hamburguer">
-        </picture>
-        <div class="title-menu">
-          <h1>&lt;GAMEE&gt;</h1>
-        </div>
-      </div>
-    </header>
     <main class="main">
-      <nav class="menu-nav">
-        <ul>
-          <li class="li-sidebar-menu"> 
-            <a href="#profile" class="itens-menu">
-              <img class="icons" src=${profileIcon} alt="profile icon">
-              <span>Meu perfil</span>
-            </a>
-          </li>
-          <li class="li-sidebar-menu"> 
-            <a href="#feed" class="itens-menu">
-              <img class="icons" src=${feedIcon} alt="feed icon">
-              <span>Feed</span>
-            </a> 
-          </li>
-          <li class="li-sidebar-menu"> 
-            <a href="" class="btn-logout" class="itens-menu">
-              <img class="icons" src=${signoutIcon} alt="signout icon">
-              <span>Sair</span>
-            </a> 
-          </li>
-        </ul>
-      </nav>
-      <nav class="menu-nav-desktop">
-        <ul>
-          <li class="li-sidebar-menu"> 
-            <a href="#profile" class="itens-menu">
-              <img class="icons profile-icon" src=${profileIcon} alt="profile icon">
-              <span>Meu perfil</span>
-            </a>
-          </li>
-          <li class="li-sidebar-menu"> 
-            <a href="#feed" class="itens-menu">
-              <img class="icons" src=${feedIcon} alt="feed icon">
-              <span>Feed</span>
-            </a> 
-          </li>
-          <li class="li-sidebar-menu"> 
-            <a href="" class="btn-logout" class="itens-menu">
-              <img class="icons" src=${signoutIcon} alt="signout icon">
-              <span>Sair</span>
-            </a> 
-          </li>
-        </ul>
-      </nav>
       <div class="feed">
         <div class="container-input-post">
           <textarea name="" id="user-text-area" placeholder="O que está jogando?"></textarea>
@@ -80,34 +26,12 @@ export default (user) => {
     </main>
     `;
   containerFeed.innerHTML = templateFeed;
+  container.appendChild(containerFeed);
 
   const feedMain = containerFeed.querySelector('.feed');
-  const btnLogOut = containerFeed.querySelector('.btn-logout');
-  const imgHamburgerMenu = containerFeed.querySelector('.hamburger-menu');
   const btnPublish = containerFeed.querySelector('.btn-publish');
-  const widthScreen = window.screen.width;
-
-  btnLogOut.addEventListener('click', async () => {
-    try {
-      await logOut();
-      window.location.href = '#home';
-    } catch (error) {
-      console.log(error.message);
-    }
-  });
-
-  imgHamburgerMenu.addEventListener('click', () => {
-    const itensMenu = containerFeed.querySelector('.menu-nav');
-    if (itensMenu.style.display === 'none') {
-      itensMenu.style.display = 'block';
-    } else if (widthScreen >= '992px') {
-      itensMenu.style.display = 'none';
-    } else {
-      itensMenu.style.display = 'none';
-    }
-  });
-
   const postsList = document.createElement('section');
+
   postsList.classList.add('section-posts');
   postsList.classList.add('section-posts');
   const showPosts = (post) => {
@@ -230,5 +154,5 @@ export default (user) => {
     });
   });
 
-  return containerFeed;
+  return container;
 };
---
+++ src/view/header/header.js
@@ -0,0 +1,98 @@
+import './header.css';
+import { logOut } from '../../firebase/firebase';
+
+import profileIcon from '../../images/profile-icon.svg';
+import signoutIcon from '../../images/signout-icon.svg';
+import feedIcon from '../../images/feed-icon.svg';
+
+export default () => {
+    const containerHeaderMenu = document.createElement('section');
+    containerHeaderMenu.classList.add('containerHeaderMenu');
+    const templateHeaderMenu = `
+    <header>
+        <div class="header">
+        <picture class="hamburger-menu">
+            <img class="icon-menu" src="images/menu-hamburger.svg" alt="menu hamburguer">
+        </picture>
+        <div class="title-menu">
+            <h1>&lt;GAMEE&gt;</h1>
+        </div>
+        </div>
+    </header>
+    <section class="menu">
+      <nav class="menu-nav">
+        <ul>
+          <li class="li-sidebar-menu"> 
+            <a href="#profile" class="itens-menu">
+                <img class="icons" src=${profileIcon} alt="profile icon">
+                <span>Meu perfil</span>
+            </a>
+          </li>
+          <li class="li-sidebar-menu"> 
+            <a href="#feed" class="itens-menu">
+              <img class="icons" src=${feedIcon} alt="feed icon">
+              <span>Feed</span>
+            </a> 
+          </li>
+          <li class="li-sidebar-menu"> 
+            <a href="" class="btn-logout" class="itens-menu">
+              <img class="icons" src=${signoutIcon} alt="signout icon">
+              <span>Sair</span>
+            </a> 
+          </li>
+        </ul>
+      </nav>
+      <nav class="menu-nav-desktop">
+        <ul>
+          <li class="li-sidebar-menu"> 
+            <a href="#profile" class="itens-menu">
+              <img class="icons profile-icon" src=${profileIcon} alt="profile icon">
+              <span>Meu perfil</span>
+            </a>
+          </li>
+          <li class="li-sidebar-menu"> 
+            <a href="#feed" class="itens-menu">
+              <img class="icons" src=${feedIcon} alt="feed icon">
+              <span>Feed</span>
+            </a> 
+          </li>
+          <li class="li-sidebar-menu"> 
+            <a href="" class="btn-logout" class="itens-menu">
+              <img class="icons" src=${signoutIcon} alt="signout icon">
+              <span>Sair</span>
+            </a> 
+          </li>
+        </ul>
+      </nav>
+    </section>
+    `
+    containerHeaderMenu.innerHTML = templateHeaderMenu;
+
+    const btnLogOut = containerHeaderMenu.querySelectorAll('.btn-logout');
+    const imgHamburgerMenu = containerHeaderMenu.querySelector('.hamburger-menu');
+    const widthScreen = window.screen.width;
+
+    btnLogOut.forEach((btn) => {
+      btn.addEventListener('click', async () => {
+        try {
+          await logOut();
+          window.location.href = '#home';
+        } catch (error) {
+          console.log(error.message);
+        }
+      });
+    });
+
+    imgHamburgerMenu.addEventListener('click', () => {
+      const itensMenu = containerHeaderMenu.querySelector('.menu-nav');
+      if (itensMenu.style.display === 'none') {
+        itensMenu.style.display = 'block';
+      } else if (widthScreen >= '992px') {
+        itensMenu.style.display = 'none';
+      } else {
+        itensMenu.style.display = 'none';
+      }
+    });
+
+    return containerHeaderMenu;
+};
