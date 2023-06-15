bcf8eaaf1f42ee17eb67f7c7edee4a1046009421
HelouiseItokazo <helouise.almeida93@gmail.com>
2023-06-15 19:07:32 -0300

    inicio do style profile

--- src/view/profile/profile.js
+++ src/view/profile/profile.js
@@ -1,28 +1,30 @@
 import './profile.css';
 import profileIcon from '../../images/profile-icon-gradient.svg';
+import header from '../header/header.js';
 
 export default () => {
+  const container = document.createElement('section');
+  container.appendChild(header());
   const containerProfile = document.createElement('section');
   containerProfile.classList.add('container-profile');
   const templateProfile = `
-    <section class="my-profile">
-    <h1>ENTROU NO PERFIL!!</h1>
-      <div class="profile-picture">
+    <section class="my-profile display-flex-column">
+      <div class="profile-picture display-flex-column">
         <img src="${profileIcon}" alt="profile icon">
         <span>Meu Perfil</span>
       </div>
-      <form>
-        <input type="text">
-        <input type="text">
-        <div>
+      <form class="display-flex-column">
+        <input type="text" placeholder="Nome">
+        <input type="text" placeholder="User">
+        <div class="btns">
           <button>Salvar</button>
           <button>Editar</button>
         </div>
       </form>
     </section>
-    <section class="posts-history">
-    </section>
+    <section class="posts-history"></section>
     `;
   containerProfile.innerHTML = templateProfile;
-  return containerProfile
+  container.appendChild(containerProfile);
+  return container;
 };
